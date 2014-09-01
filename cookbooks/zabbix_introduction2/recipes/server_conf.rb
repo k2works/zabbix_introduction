056ea2fe8a9b25aae8f68edf9db8c1dc3d92a612
k2works <kakimomokuri@gmail.com>
2014-09-01 18:34:13 +0900

    done:MySQLデータベースサーバの監視

--- cookbooks/zabbix_introduction2/recipes/server_conf.rb
+++ cookbooks/zabbix_introduction2/recipes/server_conf.rb
@@ -1,3 +1,20 @@
 include_recipe 'zabbix22::database'
 include_recipe 'zabbix22::agent'
 include_recipe 'zabbix22::service'
+
+# ODBCの設定
+%w{unixODBC mysql-connector-odbc}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+template "/etc/odbcinst.ini" do
+    source "odbcinst.ini.erb"
+    mode 0644
+end
+
+template "/etc/odbc.ini" do
+    source "odbc.ini.erb"
+    mode 0644
+end
